0b565db0c5a3b5410ba7af2bf9a90e6cb0b5b80c
soham0w0sarkar <sohamsarkar59@gmail.com>
2024-09-04 06:20:53 +0530

    adding search improvements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "../css/search.css";
+import { IconSearch } from "@tabler/icons-react"; // Importing the search icon from Tabler Icons
 
 const Search = () => {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const toggleSearchBar = () => {
+    setShowSearchBar(!showSearchBar);
+  };
+
   return (
     <div className="search-container">
-      <input
-        type="text"
-        placeholder="Search for based on Name, Title, Department, Date......"
-        className="search-bar"
-      />
+      <IconSearch className="search-icon" onClick={toggleSearchBar} />
+      <div className={`search-bar-container ${showSearchBar ? "active" : ""}`}>
+        <input
+          type="text"
+          placeholder="Search based on Name, Title, Department, Date..."
+          className="search-bar"
+        />
+      </div>
+      <div className="search-actions">
+        <button className="action-button add-button">Add Document</button>
+        <button className="action-button edit-button">Edit Document</button>
+        <button className="action-button user-button">Manage Users</button>
+      </div>
     </div>
   );
 };
